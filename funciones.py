5337ebbac78058960c7d4eb3ca247b410abcc82f
Caliz Blanco Alejo <50003651+Merok23@users.noreply.github.com>
2019-07-02 11:59:35 -0300

    Esto va a ser horrible
    hice un par de funciones, bastante seguro que ninguna funciona pero no se ni como comprobarlo, 
    tambien implementandolas surgieron un par de funciones necesarias (como obtener vertice aleatorio adyacente para random walks)

--- funciones.py
+++ funciones.py
@@ -17,6 +17,7 @@ def camino_minimo(grafo,u,v):
                 cola.append(x)
             if x == v:
                 return camino(v, padre)
+    print("Seguimiento imposible.")
     return None
 
 def camino(v, padre):
@@ -27,10 +28,63 @@ def camino(v, padre):
         lista.insert(0, j)
         j = padre[j]
     return lista
+    #No habria que imprimir el resultado?
 
+#para componentes no conexas usar un for y esto adentro.
+def bfs(grafo,vertice_origen,visitados = None):
+    visitados = {}
+    padre = {}
+    dist = {}
+    cola = deque([])
+    cola.append(vertice_origen)
+    visitados[vertice_origen] = True
+    dist[vertice_origen] = 0
+    padre[vertice_origen] = None
+    while(len(cola) != 0):
+        v = cola.popleft()
+        for w in v.adyacentes()
+            if w not in visitados:
+                visitados[w] = True
+                dist[w] = dist[v] + 1
+                padre[w] = v
+                cola.append(w)
+    return (padre,dist)
+#Esto se tiene que hacer muchas veces, y de ahi sacar los vertices por los cuales se paso
+#odio python y mi vida.
+def RandomWalks(grafo, n):
+    x = 0;
+    cola = deque([])
+    veces = {}
+    while(x != n)
+        v = grafo.obtener_vertice_aleatorio()
+        cola.append(v)
+        while(len(cola) != 0 or x != n)
+              for w in v.adyacentes(): ##Tiene que ser uno random
+                cola.append(w)
+                veces[w] += 1;
+                x +=1
+    ordenar(veces)
+    return veces
+
+
+##Este es exacto, necesitamos el aproximado (random walks) xq' para grafos grandes
+##este tarda mucho
 def PageRank_o_BTC(cant):
-    pass
-
+    cent = {}
+    distancias = {}
+    for v in grafo: cent[v] = 0
+    for v in grafo:
+        padre,distancias = bfs(grafo,v)
+        distancias.add(distancia[v])
+        cent_aux = {}
+        for w in grafo: cent_aux[w] = 0
+        vertices_ordenados = ordenar_vertices(grafo,distancias)
+        for w in vertices_ordenados:
+            cent_aux[w] = cent_aux[w] + cent_aux[w] + 1 #cent_aux[padre[w]] += 1 + cent_aux[w]
+        for w in grafo:
+            if w == v continue
+            cent[w] += cent_aux[w]
+    return cent
 
 def camino_importante(v_vertices, k):
     pass
